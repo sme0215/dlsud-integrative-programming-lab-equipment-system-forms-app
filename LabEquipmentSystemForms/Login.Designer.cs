0d5d1731e5e062c3541e017a5cc4e295e01c9756
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-05 07:31:41 +0800

    refactor, feat: Form1 to Login as MDI

---
+++ LabEquipmentSystemForms/Login.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LabEquipmentSystemForms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.userLoginMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminLoginMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.userLoginMenuItem,
+            this.adminLoginMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(980, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // userLoginMenuItem
+            // 
+            this.userLoginMenuItem.Name = "userLoginMenuItem";
+            this.userLoginMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.userLoginMenuItem.Text = "User Login";
+            // 
+            // adminLoginMenuItem
+            // 
+            this.adminLoginMenuItem.Name = "adminLoginMenuItem";
+            this.adminLoginMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.adminLoginMenuItem.Text = "Admin Login";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 557);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem userLoginMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminLoginMenuItem;
+    }
+}
+
