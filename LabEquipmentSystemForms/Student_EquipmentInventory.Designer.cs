6591af20f98368c296d3d46583db2f333b53f144
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-26 09:32:45 +0800

    feat: add equipment inventory template and form code

--- LabEquipmentSystemForms/Student_EquipmentInventory.Designer.cs
+++ LabEquipmentSystemForms/Student_EquipmentInventory.Designer.cs
@@ -29,16 +29,18 @@
         private void InitializeComponent()
         {
             this.lblFilter = new System.Windows.Forms.Label();
-            this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnRefresh = new System.Windows.Forms.Button();
-            this.btnReturn = new System.Windows.Forms.Button();
-            this.cbFilter = new System.Windows.Forms.ComboBox();
-            this.txtReturnAmount = new System.Windows.Forms.TextBox();
             this.lblReturnAmount = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.txtReturnAmount = new System.Windows.Forms.TextBox();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblEquipmentId = new System.Windows.Forms.Label();
+            this.txtEquipmentID = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,16 +56,41 @@
             this.lblFilter.Text = "FILTER BY";
             this.lblFilter.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // dataGridView
+            // lblReturnAmount
             // 
-            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView, 3);
-            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView.Location = new System.Drawing.Point(3, 138);
-            this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(874, 316);
-            this.dataGridView.TabIndex = 10;
+            this.lblReturnAmount.AutoSize = true;
+            this.lblReturnAmount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblReturnAmount.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReturnAmount.Location = new System.Drawing.Point(3, 90);
+            this.lblReturnAmount.Name = "lblReturnAmount";
+            this.lblReturnAmount.Size = new System.Drawing.Size(214, 45);
+            this.lblReturnAmount.TabIndex = 13;
+            this.lblReturnAmount.Text = "RETURN AMOUNT";
+            this.lblReturnAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtReturnAmount
+            // 
+            this.txtReturnAmount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtReturnAmount.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtReturnAmount.Location = new System.Drawing.Point(223, 96);
+            this.txtReturnAmount.Name = "txtReturnAmount";
+            this.txtReturnAmount.Size = new System.Drawing.Size(434, 32);
+            this.txtReturnAmount.TabIndex = 14;
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbFilter.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Items.AddRange(new object[] {
+            "All",
+            "Open",
+            "Closed"});
+            this.cbFilter.Location = new System.Drawing.Point(223, 3);
+            this.cbFilter.Margin = new System.Windows.Forms.Padding(3, 3, 50, 3);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(387, 33);
+            this.cbFilter.TabIndex = 16;
             // 
             // tableLayoutPanel1
             // 
@@ -71,46 +98,37 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtEquipmentID, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblEquipmentId, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 1, 3);
             this.tableLayoutPanel1.Controls.Add(this.cbFilter, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.txtReturnAmount, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.lblReturnAmount, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtReturnAmount, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblReturnAmount, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.lblFilter, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView, 0, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(880, 457);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
-            // flowLayoutPanel2
+            // dataGridView
             // 
-            this.flowLayoutPanel2.Controls.Add(this.btnRefresh);
-            this.flowLayoutPanel2.Controls.Add(this.btnReturn);
-            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(223, 93);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(434, 39);
-            this.flowLayoutPanel2.TabIndex = 17;
-            this.flowLayoutPanel2.WrapContents = false;
-            // 
-            // btnRefresh
-            // 
-            this.btnRefresh.AutoSize = true;
-            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRefresh.Location = new System.Drawing.Point(3, 3);
-            this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(106, 34);
-            this.btnRefresh.TabIndex = 16;
-            this.btnRefresh.Text = "REFRESH";
-            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.dataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLayoutPanel1.SetColumnSpan(this.dataGridView, 3);
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(3, 183);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(874, 271);
+            this.dataGridView.TabIndex = 10;
+            this.dataGridView.SelectionChanged += new System.EventHandler(this.dataGridView_SelectionChanged);
             // 
             // btnReturn
             // 
@@ -124,43 +142,54 @@
             this.btnReturn.TabIndex = 17;
             this.btnReturn.Text = "RETURN SELECTED";
             this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
             // 
-            // cbFilter
+            // btnRefresh
             // 
-            this.cbFilter.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.cbFilter.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cbFilter.FormattingEnabled = true;
-            this.cbFilter.Items.AddRange(new object[] {
-            "All",
-            "Borrowed",
-            "Returned",
-            "Lost"});
-            this.cbFilter.Location = new System.Drawing.Point(223, 3);
-            this.cbFilter.Margin = new System.Windows.Forms.Padding(3, 3, 50, 3);
-            this.cbFilter.Name = "cbFilter";
-            this.cbFilter.Size = new System.Drawing.Size(387, 33);
-            this.cbFilter.TabIndex = 16;
+            this.btnRefresh.AutoSize = true;
+            this.btnRefresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnRefresh.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefresh.Location = new System.Drawing.Point(3, 3);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(106, 34);
+            this.btnRefresh.TabIndex = 16;
+            this.btnRefresh.Text = "REFRESH";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
-            // txtReturnAmount
+            // flowLayoutPanel2
             // 
-            this.txtReturnAmount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtReturnAmount.Font = new System.Drawing.Font("Segoe UI", 14F);
-            this.txtReturnAmount.Location = new System.Drawing.Point(223, 51);
-            this.txtReturnAmount.Name = "txtReturnAmount";
-            this.txtReturnAmount.Size = new System.Drawing.Size(434, 32);
-            this.txtReturnAmount.TabIndex = 14;
+            this.flowLayoutPanel2.Controls.Add(this.btnRefresh);
+            this.flowLayoutPanel2.Controls.Add(this.btnReturn);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(223, 138);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(434, 39);
+            this.flowLayoutPanel2.TabIndex = 17;
+            this.flowLayoutPanel2.WrapContents = false;
             // 
-            // lblReturnAmount
+            // lblEquipmentId
             // 
-            this.lblReturnAmount.AutoSize = true;
-            this.lblReturnAmount.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblReturnAmount.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblReturnAmount.Location = new System.Drawing.Point(3, 45);
-            this.lblReturnAmount.Name = "lblReturnAmount";
-            this.lblReturnAmount.Size = new System.Drawing.Size(214, 45);
-            this.lblReturnAmount.TabIndex = 13;
-            this.lblReturnAmount.Text = "RETURN AMOUNT";
-            this.lblReturnAmount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblEquipmentId.AutoSize = true;
+            this.lblEquipmentId.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblEquipmentId.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipmentId.Location = new System.Drawing.Point(3, 45);
+            this.lblEquipmentId.Name = "lblEquipmentId";
+            this.lblEquipmentId.Size = new System.Drawing.Size(214, 45);
+            this.lblEquipmentId.TabIndex = 19;
+            this.lblEquipmentId.Text = "EQUIPMENT ID";
+            this.lblEquipmentId.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtEquipmentID
+            // 
+            this.txtEquipmentID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtEquipmentID.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtEquipmentID.Location = new System.Drawing.Point(223, 51);
+            this.txtEquipmentID.Name = "txtEquipmentID";
+            this.txtEquipmentID.ReadOnly = true;
+            this.txtEquipmentID.Size = new System.Drawing.Size(434, 32);
+            this.txtEquipmentID.TabIndex = 21;
             // 
             // FormStudentEquipmentInventory
             // 
@@ -171,9 +200,9 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "FormStudentEquipmentInventory";
             this.Text = "Equipment Inventory";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.flowLayoutPanel2.ResumeLayout(false);
             this.flowLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
@@ -183,13 +212,15 @@
         #endregion
 
         private System.Windows.Forms.Label lblFilter;
-        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label lblReturnAmount;
+        private System.Windows.Forms.TextBox txtReturnAmount;
+        private System.Windows.Forms.ComboBox cbFilter;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.TextBox txtEquipmentID;
+        private System.Windows.Forms.Label lblEquipmentId;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Button btnRefresh;
         private System.Windows.Forms.Button btnReturn;
-        private System.Windows.Forms.ComboBox cbFilter;
-        private System.Windows.Forms.TextBox txtReturnAmount;
-        private System.Windows.Forms.Label lblReturnAmount;
     }
 }
--- LabEquipmentSystemForms/Student_EquipmentInventory.cs
+++ LabEquipmentSystemForms/Student_EquipmentInventory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataHelper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,146 @@ namespace LabEquipmentSystemForms
 {
     public partial class FormStudentEquipmentInventory : Form
     {
-        public FormStudentEquipmentInventory()
+        private string studentID;
+        private BindingSource bs = new BindingSource();
+
+        public FormStudentEquipmentInventory(string studentID)
         {
             InitializeComponent();
+            this.studentID = studentID;
+            LoadStudentInventory();
+        }
+
+        private void LoadStudentInventory()
+        {
+            try
+            {
+                // Assuming you have a method to get student's inventory
+                DataTable dt = DataAccess.GetStudentInventory(studentID);
+                bs.DataSource = dt;
+                dataGridView.DataSource = bs;
+
+                // Set up data binding for textboxes if needed
+                BindDataSourceToControls();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading inventory: {ex.Message}", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BindDataSourceToControls()
+        {
+            txtEquipmentID.DataBindings.Clear();
+
+            txtEquipmentID.DataBindings.Add("Text", bs, "EquipmentID");
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.CurrentRow == null)
+            {
+                MessageBox.Show("Please select an item to return.", "Selection Required",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                // Get data from selected row
+                string requestID = dataGridView.CurrentRow.Cells["RequestID"].Value?.ToString();
+                string currentStudentID = dataGridView.CurrentRow.Cells["StudentID"].Value?.ToString();
+                string status = dataGridView.CurrentRow.Cells["Status"].Value?.ToString();
+
+                int returnAmount = Convert.ToInt32(txtReturnAmount.Text.ToString());
+                int remainingQty = Convert.ToInt32(dataGridView.CurrentRow.Cells["RemainingQuantity"].Value);
+
+                // Validate inputs
+                if (string.IsNullOrEmpty(requestID))
+                {
+                    MessageBox.Show("Invalid request ID.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (status == "CLOSED")
+                {
+                    MessageBox.Show("This item has already been fully returned and closed.",
+                                  "Already Returned", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                if (returnAmount <= 0)
+                {
+                    MessageBox.Show("Return amount must be greater than 0.",
+                                  "Invalid Amount", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (returnAmount > remainingQty)
+                {
+                    MessageBox.Show($"Return amount cannot exceed remaining quantity ({remainingQty}).",
+                                  "Invalid Amount", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Confirm return
+                DialogResult confirm = MessageBox.Show(
+                    $"Are you sure you want to return {returnAmount} item(s)?",
+                    "Confirm Return",
+                    MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+
+                if (confirm != DialogResult.Yes)
+                    return;
+
+                // Process return
+                bool success = DataAccess.ReturnEquipment(requestID, currentStudentID, returnAmount);
+
+                if (success)
+                {
+                    MessageBox.Show("Equipment returned successfully!", "Success",
+                                  MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Reset return amount
+                    txtReturnAmount.Text = "0";
+
+                    // Refresh the inventory
+                    LoadStudentInventory();
+                }
+                else
+                {
+                    MessageBox.Show($"Failed to return equipment.", "Error",
+                                  MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error",
+                              MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadStudentInventory();
+            MessageBox.Show("Inventory refreshed.", "Refresh",
+                          MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void dataGridView_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView.CurrentRow != null)
+            {
+                bs.Position = dataGridView.CurrentRow.Index;
+
+                // Update the maximum return amount based on remaining quantity
+                if (dataGridView.CurrentRow.Cells["RemainingQuantity"].Value != null)
+                {
+                    int remainingQty = Convert.ToInt32(dataGridView.CurrentRow.Cells["RemainingQuantity"].Value);
+                    txtReturnAmount.Text = remainingQty.ToString();
+                }
+            }
         }
     }
 }
